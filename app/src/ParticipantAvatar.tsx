7ee5f5894739b42ed24eeeab7c7fd44a47e0da87
Stephanie Wang <steph.wang@discordapp.com>
2025-10-21 17:03:13 -0400

    add participant avatar component

---
+++ app/src/ParticipantAvatar.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { type Participant } from "./useDiscordSdk";
+
+
+export function ParticipantAvatar({participant, size}: {participant: Participant, size: number}) {
+return <img
+    src={
+      participant.avatar != null
+        ? `https://cdn.discordapp.com/avatars/${participant.id}/${participant.avatar}.png?size=256`
+        : `https://cdn.discordapp.com/embed/avatars/${
+            (BigInt(participant.id) >> 22n) % 6n
+          }.png`
+    }
+    alt="avatar"
+    width={size}
+    height={size}
+  />
+}
