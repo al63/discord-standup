f6bab437bae90e5f07afd3f893250de90b610f70
Alec Lee <alec.lee@discordapp.com>
2025-10-23 14:38:25 -0700

    swap out font

---
+++ app/src/Popcorn.tsx
@@ -0,0 +1,86 @@
+import "./App.css";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useReward } from "partycles";
+interface Popcorn {
+  [id: number]: { x: number; y: number };
+}
+function Popcorn({ id, style }: { id: string; style: React.CSSProperties }) {
+  // @ts-expect-error the emoji animation config is documented wrong
+  const { reward } = useReward(`popcorn-${id}`, "emoji", {
+    emojis: ["🍿"],
+    particleCount: 1,
+  });
+
+  useEffect(() => {
+    reward();
+  }, [reward]);
+
+  return <div id={`popcorn-${id}`} className="popcorn" style={style} />;
+}
+
+export function PopcornContainer({
+  websocket,
+}: {
+  websocket: WebSocket | null;
+}) {
+  const id = useRef(0);
+  const [popcorn, setPopcorn] = useState<Popcorn>({});
+  const init = useRef(false);
+
+  const handlePopcorn = useCallback(({ x, y }: { x: number; y: number }) => {
+    const currentId = id.current;
+    const boundingRect = document.body.getBoundingClientRect();
+    setPopcorn((popcorn) => {
+      return {
+        ...popcorn,
+        [currentId]: { x: x * boundingRect.width, y: y * boundingRect.height },
+      };
+    });
+    setTimeout(() => {
+      setPopcorn((popcorn) => {
+        const newPopcorn = { ...popcorn };
+        delete newPopcorn[currentId];
+        return newPopcorn;
+      });
+    }, 1000);
+    id.current++;
+  }, []);
+
+  useEffect(() => {
+    if (init.current === true) {
+      return;
+    }
+
+    if (websocket == null) {
+      return;
+    }
+
+    init.current = true;
+    document.body.addEventListener("click", (e) => {
+      const boundingRect = document.body.getBoundingClientRect();
+      websocket?.send(
+        JSON.stringify({
+          type: "popcorn",
+          x: (e.clientX / boundingRect.width).toFixed(2),
+          y: (e.clientY / boundingRect.height).toFixed(2),
+        })
+      );
+    });
+
+    websocket?.addEventListener("message", (event) => {
+      const parsed = JSON.parse(event.data);
+
+      if (parsed.type === "popcorn") {
+        handlePopcorn({ x: parsed.x, y: parsed.y });
+      }
+    });
+  }, [handlePopcorn, websocket]);
+
+  return (
+    <>
+      {Object.entries(popcorn).map(([id, { x, y }]) => (
+        <Popcorn id={id} key={id} style={{ left: x, top: y }} />
+      ))}
+    </>
+  );
+}
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "./fonts/NunitoSans.ttf";
+import "./fonts/ComicNeue-Regular.ttf";
+import "./fonts/ComicNeue-Bold.ttf";
 import "./fonts/PermanentMarker.ttf";
 
 createRoot(document.getElementById("root")!).render(<App />);
