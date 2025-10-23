f7fc01218fd91eb514fe15d81fecba1a723de3a1
Alec Lee <alec.lee@discordapp.com>
2025-10-23 11:29:32 -0700

    lol

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,6 +4,46 @@ import { WaitingRoom } from "./WaitingRoom";
 import { useStandupWebsocket } from "./useWebsocket";
 import { Standup } from "./Standup";
 import { LoadingScreen } from "./LoadingScreen";
+import { useEffect, useRef, useState } from "react";
+
+interface Popcorn {
+  [id: number]: { x: number; y: number };
+}
+
+function PopcornContainer() {
+  const id = useRef(0);
+  const [popcorn, setPopcorn] = useState<Popcorn>({});
+  const init = useRef(false);
+
+  useEffect(() => {
+    if (init.current === true) {
+      return;
+    }
+
+    init.current = true;
+    document.body.addEventListener("click", (e) => {
+      console.log(e.clientX, e.clientY);
+      setPopcorn((popcorn) => {
+        return {
+          ...popcorn,
+          [id.current]: { x: e.clientX, y: e.clientY },
+        };
+      });
+      id.current++;
+    });
+  }, [popcorn]);
+
+  /*
+  return (
+    <>
+      {Object.entries(popcorn).map(([id, { x, y }]) => (
+        <div key={id} className="popcorn" style={{ left: x, top: y }} />
+      ))}
+    </>
+  );
+  */
+  return null;
+}
 
 function App() {
   const { participants, discordSdk, auth } = useDiscordSdk();
@@ -13,13 +53,15 @@ function App() {
     return <LoadingScreen />;
   } else if (standupState.type === "pending") {
     return (
-      <WaitingRoom
-        participants={participants}
-        standupState={standupState}
-        discordSdk={discordSdk}
-        currentUser={auth.user}
-        websocket={websocket}
-      />
+      <>
+        <WaitingRoom
+          participants={participants}
+          standupState={standupState}
+          discordSdk={discordSdk}
+          currentUser={auth.user}
+          websocket={websocket}
+        />
+      </>
     );
   } else {
     return (
@@ -33,4 +75,13 @@ function App() {
   }
 }
 
-export default App;
+const AppContainer = () => {
+  return (
+    <>
+      <PopcornContainer />
+      <App />
+    </>
+  );
+};
+
+export default AppContainer;
