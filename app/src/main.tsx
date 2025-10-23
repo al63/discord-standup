779cb147c2cacd9ce7567be2860d3952b06f9513
Stephanie Wang <steph.wang@discordapp.com>
2025-10-23 10:49:34 -0400

    fonts...?

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import "./fonts/NunitoSans.ttf";
+import "./fonts/PermanentMarker.ttf";
 
 createRoot(document.getElementById("root")!).render(<App />);
